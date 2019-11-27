3b111b2e54cd9fd2b6fb5913f68f4aa6ed06e1e3
Maylee <mgagnon987@gmail.com>
2019-11-27 14:25:37 -0500

    started code for hashcode

--- hashing/src/hashing.cpp
+++ hashing/src/hashing.cpp
@@ -3,9 +3,57 @@
  * 11.26.2019
  */
 #include <iostream>
+#include <cstring>
+#include <fstream>
+
 using namespace std;
 
+#define C 123
+#define m 1000
+
+/*
+ *
+ */
+int ord(char c){
+	if (isalpha(c)){
+		return int(c);
+	}
+	else if (c == '\''){
+		return 39;
+	}
+	return -1;
+}
+
+/*
+ *
+ */
+int createHash(char* str){ //TODO how to pass in string properly
+	int hash = 0;
+	for (int i = 0; i < strlen(str); i++){
+		hash = (hash*C +ord(str[i]))%m;
+	}
+	return hash;
+}
+
+/*
+ *
+ */
+bool addToTable(){
+
+}
+
 int main() {
-	cout << "Hi" << endl;
+	int testHash = createHash("hello");
+	cout << testHash;
+
+	ifstream fin;
+	fin.open("filename here");
+	while(fin){
+		//get a word, (print to test)
+		//call createHash
+		//call add to table, discard duplicates, flag for words not in first location thing when setting up table
+	}
+	fin.close();
+
 	return 0;
 }
